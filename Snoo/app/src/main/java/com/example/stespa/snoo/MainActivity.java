6d52cfb17c16c4852a22a96dc782dca45180eb95
Stefan Spasov <stespa@jetshop.se>
2017-02-26 01:10:35 +0100

    Refactoring

--- Snoo/app/src/main/java/com/example/stespa/snoo/MainActivity.java
+++ Snoo/app/src/main/java/com/example/stespa/snoo/MainActivity.java
@@ -9,36 +9,45 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.example.stespa.snoo.helpers.DividerItemDecoration;
+import com.example.stespa.snoo.helpers.EntriesAdapter;
+import com.example.stespa.snoo.helpers.RecyclerTouchListener;
+import com.example.stespa.snoo.models.RedditEntry;
+
 import java.util.ArrayList;
 
+import static android.support.v7.widget.RecyclerView.LayoutManager;
+
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private ListAdapter eAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Bundle extras = getIntent().getExtras();
-        final   ArrayList<RedditEntry> list = (ArrayList<RedditEntry>)extras.getSerializable("entries");
+        final ArrayList<RedditEntry> entries = (ArrayList<RedditEntry>) extras.getSerializable("entries");
 
-        recyclerView = (RecyclerView) findViewById(R.id.reddit_entries_view);
-        eAdapter = new ListAdapter(list);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.reddit_entries_view);
+        EntriesAdapter entriesAdapter = new EntriesAdapter(entries);
+        LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
-        recyclerView.setAdapter(eAdapter);
+        recyclerView.setAdapter(entriesAdapter);
         recyclerView.addOnItemTouchListener(
-               new RecyclerTouchListener(getApplicationContext() ,new RecyclerTouchListener.OnItemClickListener() {
-                     @Override public void onItemClick(View view, int position) {
-                           RedditEntry redditEntry= list.get(position);
-                           // Fixes "URI signature match failed" bug with Reddit
-                           String newUrl = redditEntry.getUrl().replaceAll("&amp;", "&");
-                           Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(newUrl));
-                           startActivity(browserIntent);
-                      }
-               })
+                new RecyclerTouchListener(getApplicationContext(), new RecyclerTouchListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        if (entries != null) {
+                            RedditEntry redditEntry = entries.get(position);
+                            // Fixes "URI signature match failed" bug with Reddit
+                            String newUrl = redditEntry.getUrl().replaceAll("&amp;", "&");
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(newUrl));
+                            startActivity(browserIntent);
+                        }
+                    }
+                })
         );
     }
 }
--- Snoo/app/src/main/java/com/example/stespa/snoo/Splash.java
+++ Snoo/app/src/main/java/com/example/stespa/snoo/Splash.java
@@ -1,9 +1,20 @@
 package com.example.stespa.snoo;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.stespa.snoo.helpers.RestClient;
+import com.example.stespa.snoo.models.RedditEntry;
+import com.example.stespa.snoo.models.RedditEntryJson;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import java.net.URLEncoder;
 import java.util.ArrayList;
 
@@ -12,44 +23,71 @@ import java.util.ArrayList;
  */
 
 public class Splash extends Activity {
+    private String mUrl = "https://www.reddit.com/r/gaming/top.json";
+    private String mLimit = "50";
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        String url = "https://www.reddit.com/r/gaming/top.json";
-        String queryParams = "";
-        String charset = "UTF-8";
-        String limit ="20";
+
+        if (!isConnected()) {
+            Toast.makeText(getApplicationContext(), "Network is unavailable.", Toast.LENGTH_LONG).show();
+        } else {
+            new RestClient() {
+                @Override
+                public void onPostExecute(String result) {
+                    try {
+                        Intent openStart = new Intent("android.intent.action.MAINPAGE");
+                        openStart.putExtra("entries", getRedditEntriesFromResult(result));
+                        startActivity(openStart);
+                    } catch (Exception e) {
+                        Log.e("splash", "Cannot open main page", e);
+                    }
+                }
+            }.execute(mUrl, getQueryParams());
+        }
+    }
+
+    private ArrayList<RedditEntry> getRedditEntriesFromResult(String result) {
+        ArrayList<RedditEntry> entries = new ArrayList<>();
+        Gson gson = new GsonBuilder().create();
         try {
-            queryParams = String.format("limit=%s",
-                    URLEncoder.encode(limit, charset));
-        }
-        catch (Exception e) {
-         // TODO LOG
-        }
+            RedditEntryJson redditJson = gson.fromJson(result, RedditEntryJson.class);
 
-        new RestClient()
-        {
-            @Override
-            public void onPostExecute(String result)
-            {
-                try {
-                    ArrayList<RedditEntry> entries = new ArrayList<RedditEntry>();
-                    Gson gson = new GsonBuilder().create();
-
-                    RedditEntryJson redditJson = gson.fromJson(result, RedditEntryJson.class);
-                    for (RedditEntryJson.Child entry : redditJson.data.children) {
-                        RedditEntry newEntry = new RedditEntry(entry.data.title, entry.data.score, entry.data.subreddit, entry.data.url);
+            if (redditJson != null && redditJson.data != null) {
+                for (RedditEntryJson.Child entry : redditJson.data.children) {
+                    if (entry.data != null) {
+                        RedditEntry newEntry = new RedditEntry(
+                                entry.data.title,
+                                entry.data.score,
+                                entry.data.subreddit,
+                                entry.data.url);
                         entries.add(newEntry);
                     }
-                    Intent openStart = new Intent("android.intent.action.MAINPAGE");
-                    openStart.putExtra("entries", entries);
-                    startActivity(openStart);
-                }
-                catch (Exception e)
-                {
-                    // TODO log
                 }
             }
-        }.execute(new String[]{url, queryParams});
+
+            return entries;
+        } catch (Exception e) {
+            Log.e("splash", "Cannot parse reddit entries", e);
+            return entries;
+        }
+    }
+
+    private String getQueryParams() {
+        String charset = "UTF-8";
+        try {
+            return String.format("limit=%s", URLEncoder.encode(mLimit, charset));
+        } catch (Exception e) {
+            Log.e("splash", "Cannot get query parameters", e);
+            return "";
+        }
+    }
+
+    private boolean isConnected()
+    {
+        ConnectivityManager cm = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 }
